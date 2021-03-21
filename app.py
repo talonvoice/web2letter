4d1db36d79df93233d850e5d7b94ba9623c9d1b6
Ryan Hileman <lunixbochs@gmail.com>
2021-03-20 19:23:38 -0700

    basic conformer support

--- app.py
+++ app.py
@@ -10,68 +10,92 @@ import time
 import zlib
 
 if os.name == 'nt':
-    w2l_library = 'libw2l.dll'
+    libext = 'dll'
 elif os.name == 'posix':
     if sys.platform == 'darwin':
-        w2l_library = 'libw2l.dylib'
+        libext = 'dylib'
     else:
-        w2l_library = 'libw2l.so'
+        libext = 'so'
 else:
     raise RuntimeError('unsupported OS')
+libw2l = 'lib/libw2l.{}'.format(libext)
+libw2ldecode = 'lib/libw2ldecode.{}'.format(libext)
+
+def read_header(path):
+    with open(path, 'r') as f:
+        lines = []
+        ifdefs = 0
+        for line in f.read().split('\n'):
+            line = line.strip()
+            line = line.replace('__attribute__((packed))', '')
+            if line.startswith('#ifdef'):
+                ifdefs += 1
+            elif line.startswith('#endif'):
+                ifdefs -= 1
+            elif ifdefs == 0 and not line.startswith('#'):
+                lines.append(line)
+        return '\n'.join(lines)
+
+common_h = read_header('include/w2l_common.h')
+encode_h = read_header('include/w2l_encode.h')
+decode_h = read_header('include/w2l_decode.h')
 
 ffi = cffi.FFI()
 ffi.cdef('void free(void *);')
-with open('w2l.h', 'r') as f:
-    lines = []
-    ifdefs = 0
-    for line in f.read().split('\n'):
-        line = line.strip()
-        if line.startswith('#ifdef'):
-            ifdefs += 1
-        elif line.startswith('#endif'):
-            ifdefs -= 1
-        elif ifdefs == 0 and not line.startswith('#'):
-            lines.append(line)
-    header = '\n'.join(lines)
-    ffi.cdef(header)
-lib = ffi.dlopen(w2l_library)
+ffi.cdef(common_h)
+ffi.cdef(encode_h)
+lib = ffi.dlopen(libw2l)
+
+decodeffi = cffi.FFI()
+decodeffi.cdef(common_h)
+decodeffi.cdef(decode_h)
+decodelib = decodeffi.dlopen(libw2ldecode)
 
 good = True
-for name in ('acoustic.bin', 'tokens.txt', 'lm-ngram.bin', 'lexicon.txt'):
+for name in ('model/acoustic.b2l', 'model/tokens.txt', 'model/lm-ngram.bin', 'model/lexicon.txt'):
     if not os.path.exists(name):
         print('Error: {} not found.'.format(name))
         good = False
 if not good:
     sys.exit(1)
 
-if not os.path.exists('lexicon_flat.bin'):
-    lib.w2l_make_flattrie(b'tokens.txt', b'lm-ngram.bin',
-                          b'lexicon.txt', b'lexicon_flat.bin')
-
 # set up decode options
-decode_opts = ffi.new('w2l_decode_options *')
-decode_opts.beamsize = 183
-decode_opts.beamthresh = 23.530
-decode_opts.lmweight = 1.30
-decode_opts.wordscore = 0.5
-decode_opts.unkweight = -float('Inf')
-decode_opts.logadd = False
-decode_opts.silweight = 0
+opts = decodeffi.new('w2l_decode_options *')
 
-dfa_opts = ffi.new('w2l_dfa_decode_options *')
-dfa_opts.command_score = 0.5
-dfa_opts.rejection_threshold = 0.55
-dfa_opts.rejection_window_frames = 8
-dfa_opts.debug = False
+opts.beamsize = 183
+opts.beamthresh = 23.530
+opts.lmweight = 1.30
+opts.wordscore = 0.5
+opts.unkweight = -float('Inf')
+opts.logadd = False
+opts.silweight = 0
+
+c_criterion = ffi.new('char[]', b'ctc') # FIXME
+opts.criterion = c_criterion
+
+opts.command_score = 0.5
+opts.rejection_threshold = 0.55
+opts.rejection_window_frames = 8
+opts.debug = False
 # end decode opts
 
 encoder_tokens = []
-with open('tokens.txt', 'r') as f:
+with open('model/tokens.txt', 'r') as f:
     for line in f:
         encoder_tokens.append(line.strip())
+tokens = '\n'.join(encoder_tokens)
 
-encoder = lib.w2l_engine_new(b'acoustic.bin', b'tokens.txt')
-decoder = lib.w2l_decoder_new(encoder, b'lm-ngram.bin', b'lexicon.txt', b'lexicon_flat.bin', decode_opts)
+encoder = lib.w2l_engine_new()
+if not encoder or not lib.w2l_engine_load_b2l(encoder, b'model/acoustic.b2l'):
+    raise RuntimeError('failed to load model')
+decoder = decodelib.w2l_decoder_new(tokens.encode('utf8'), b'model/lm-ngram.bin', b'model/lexicon.txt', opts)
+
+c_trie_path = b'model/lexicon-flat.bin'
+if not decodelib.w2l_decoder_load_trie(decoder, c_trie_path):
+    if not decodelib.w2l_decoder_make_trie(decoder, c_trie_path):
+        raise RuntimeError('trie creation failed')
+    if not decodelib.w2l_decoder_load_trie(decoder, c_trie_path):
+        raise RuntimeError('trie load failed in w2l_decoder_load_trie()')
 
 def consume_c_text(c_text, sep):
     if not c_text:
@@ -85,45 +109,39 @@ def consume_c_text(c_text, sep):
 def w2l_decode(samples, dfa=None):
     c_samples = ffi.new('float[]', samples)
     start = time.monotonic()
-    emission = lib.w2l_engine_process(encoder, c_samples, len(samples))
+    emission = lib.w2l_engine_forward(encoder, c_samples, len(samples))
+    emission = decodeffi.cast('w2l_emission *', emission)
     emit_ms = (time.monotonic() - start) * 1000
 
-    emit_text = lib.w2l_emission_text(emission)
-    emit = consume_c_text(emit_text, sep=' ')
+    emit_text = decodelib.w2l_decoder_greedy(decoder, emission)
+    emit = consume_c_text(emit_text, sep='|')
     if not emit:
-        lib.w2l_emission_free(emission)
-        return [], [], emit_ms, 0
+        lib.free(emission)
+        return '', [], emit_ms, 0
+    return ' '.join(emit), emit, emit_ms, 0
 
     start = time.monotonic()
     if dfa:
         dfa_node = ffi.cast('w2l_dfa_node *', ffi.from_buffer(dfa))
-        decode_text = lib.w2l_decoder_dfa(encoder, decoder, emission, dfa_node, dfa_opts)
+        decode_text = decodelib.w2l_decoder_dfa(decoder, emission, dfa_node, len(dfa))
     else:
-        decode_result = lib.w2l_decoder_decode(decoder, emission)
-        decode_text = lib.w2l_decoder_result_words(decoder, decode_result)
-        lib.w2l_decoderesult_free(decode_result)
+        decode_text = decodelib.w2l_decoder_decode(decoder, emission)
     decode_ms = (time.monotonic() - start) * 1000
-    lib.w2l_emission_free(emission)
+    lib.free(emission)
 
     decode = consume_c_text(decode_text, sep=' ')
     return emit, decode, emit_ms, decode_ms
 
-app = Flask('wav2letter')
+app = Flask('web2letter')
 
 @app.route('/')
 def slash():
-    # TODO: ship webrtcvad here and let people play with it interactively?
     return render_template('index.html')
 
 # TODO: use /info in web UI?
 @app.route('/info')
 def info():
-    return jsonify({'version': 2, 'tokens': encoder_tokens})
-
-# TODO: tokens is deprecated
-@app.route('/tokens')
-def tokens():
-    return jsonify({'tokens': encoder_tokens})
+    return jsonify({'version': 3, 'tokens': encoder_tokens})
 
 @app.route('/stats')
 def stats():
